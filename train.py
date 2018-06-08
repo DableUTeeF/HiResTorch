47337e9d19b5f5d19b19b4294861aeb8c490b961
Natthaphon Hongcharoen <palm22180@gmail.com>
2018-06-08 14:16:28 +0700

    Finished model.py. Added viz 	modified:   models.py 	modified:   train.py

--- train.py
+++ train.py
@@ -10,17 +10,18 @@ import torch.utils.data.distributed
 import torchvision.transforms as transforms
 from torchvision.datasets.cifar import CIFAR10
 from utils import progress_bar, summary
-# from models import *
-from hardcodedmodels import *
+from models import HiResC
+from resnet20 import ResNet, BasicBlock
+# from hardcodedmodels import *
 from torch.optim.lr_scheduler import MultiStepLR
 
-try_no = 5
-
+try_no = [7, '110']
+mods = [HiResC([18, 18, 18]), ResNet(BasicBlock, [18, 18, 18])]
 
 if __name__ == '__main__':
     log = {'acc': [], 'loss': [], 'val_acc': [], 'val_loss': []}
     best_acc = 0
-    start_epoch = 0
+    start_epoch = 1
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
@@ -40,81 +41,83 @@ if __name__ == '__main__':
     testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
     device = 'cuda'
-    # model = HiResC(BasicBlock, [3, 3, 3])
-    model = HiResC(1)
-    model = torch.nn.DataParallel(model).cuda()
-    summary((3, 32, 32), model)
-    criterion = nn.CrossEntropyLoss().cuda()
-    optimizer = torch.optim.SGD(model.parameters(), 1e-2,
-                                momentum=0.9,
-                                weight_decay=1e-6, nesterov=True)
-    scheduler = MultiStepLR(optimizer, milestones=[150, 225])
-    cudnn.benchmark = True
+    for elm in range(len(try_no)):
+        n = try_no[elm]
+        model = mods[elm]
+        # model = HiResC(1)
+        model = torch.nn.DataParallel(model).cuda()
+        # summary((3, 32, 32), model)
+        criterion = nn.CrossEntropyLoss().cuda()
+        optimizer = torch.optim.SGD(model.parameters(), 1e-2,
+                                    momentum=0.9,
+                                    weight_decay=1e-6, nesterov=True)
+        scheduler = MultiStepLR(optimizer, milestones=[150, 225])
+        cudnn.benchmark = True
 
-    def train(epoch):
-        print('\nEpoch: %d' % epoch)
-        scheduler.step()
-        model.train()
-        train_loss = 0
-        correct = 0
-        total = 0
-        for batch_idx, (inputs, targets) in enumerate(trainloader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-
-            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-        log['acc'].append(100.*correct/total)
-        log['loss'].append(train_loss/(batch_idx+1))
-
-    def test(epoch):
-        global best_acc
-        model.eval()
-        test_loss = 0
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for batch_idx, (inputs, targets) in enumerate(testloader):
+        def train(epoch):
+            print('\nEpoch: %d' % epoch)
+            scheduler.step()
+            model.train()
+            train_loss = 0
+            correct = 0
+            total = 0
+            for batch_idx, (inputs, targets) in enumerate(trainloader):
                 inputs, targets = inputs.to(device), targets.to(device)
+                optimizer.zero_grad()
                 outputs = model(inputs)
                 loss = criterion(outputs, targets)
+                loss.backward()
+                optimizer.step()
 
-                test_loss += loss.item()
+                train_loss += loss.item()
                 _, predicted = outputs.max(1)
                 total += targets.size(0)
                 correct += predicted.eq(targets).sum().item()
 
-                progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                    % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
-        log['val_acc'].append(100.*correct/total)
-        log['val_loss'].append(test_loss/(batch_idx+1))
+                progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%%'
+                    % (train_loss/(batch_idx+1), 100.*correct/total))
+            log['acc'].append(100.*correct/total)
+            log['loss'].append(train_loss/(batch_idx+1))
 
-        # Save checkpoint.
-        acc = 100.*correct/total
-        if acc > best_acc:
-            print('Saving..')
-            state = {
-                'net': model.state_dict(),
-                'acc': acc,
-                'epoch': epoch,
-            }
-            if not os.path.isdir('checkpoint'):
-                os.mkdir('checkpoint')
-            torch.save(state, './checkpoint/ckpt.t7')
-            best_acc = acc
+        def test(epoch):
+            global best_acc
+            model.eval()
+            test_loss = 0
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for batch_idx, (inputs, targets) in enumerate(testloader):
+                    inputs, targets = inputs.to(device), targets.to(device)
+                    outputs = model(inputs)
+                    loss = criterion(outputs, targets)
+
+                    test_loss += loss.item()
+                    _, predicted = outputs.max(1)
+                    total += targets.size(0)
+                    correct += predicted.eq(targets).sum().item()
+
+                    progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%%'
+                        % (test_loss/(batch_idx+1), 100.*correct/total))
+            log['val_acc'].append(100.*correct/total)
+            log['val_loss'].append(test_loss/(batch_idx+1))
+
+            # Save checkpoint.
+            acc = 100.*correct/total
+            if acc > best_acc:
+                print('Saving..')
+                state = {
+                    'net': model.state_dict(),
+                    'acc': acc,
+                    'epoch': epoch,
+                }
+                if not os.path.isdir('checkpoint'):
+                    os.mkdir('checkpoint')
+                torch.save(state, './checkpoint/ckpt.t7')
+                best_acc = acc
 
 
-    for epoch in range(start_epoch, start_epoch+300):
-        train(epoch)
-        test(epoch)
-    with open('log/try_{}.json'.format(try_no), 'w') as wr:
-        wr.write(log.__str__())
+        for epoch in range(start_epoch, start_epoch+300):
+            train(epoch)
+            test(epoch)
+        with open('log/try_{}.json'.format(n), 'w') as wr:
+            wr.write(log.__str__())
